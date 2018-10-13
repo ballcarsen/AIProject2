d06a787688fda8c71a4e553bae2bb30ac347b42e
Brett <Leasure57@gmail.com>
2018-10-12 22:16:00 -0600

    more comments

--- BackTracking/Driver.py
+++ BackTracking/Driver.py
@@ -4,8 +4,9 @@ from BackTracking.BackTrackingSearch import BackTrackingSearch
 if __name__ == '__main__':
     # TODO: read in graphState, and fill in color character array
 
-    mockColors = ["R", "G", "B"]
+    mockColors = ["R", "G", "B"] # just a list of the colors to connect
 
+    # In actuality, I think the graph state will be a Map data type from your code
     mockGraphState = [["B","_", "G"],
                       ["_", "R", "_"],
                       ["B", "R", "G"]]
